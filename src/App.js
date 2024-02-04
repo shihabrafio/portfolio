c04e329af0802ea35a12e47017e9f651af7a9946
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-02-05 02:12:22 +0600

    Update loading spinner text

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
 
                     <div className='loading-pag'>
                         <div className="loader">
-                            <span>=(Nahuel61920)=></span>
-                            <span>=(Nahuel61920)=></span>
+                            <span>=(Rafio)=></span>
+                            <span>=(Rafio)=></span>
                         </div>
                     </div>
 
